903c6d3310b330225b630ac7bcd144f70ff84326
MD Saif Ahmed Shishir <shishirsaif216837@gmail.com>
2026-04-17 20:25:08 +0600

    task_management_provider.dart

--- lib/providers/task_management_provider.dart
+++ lib/providers/task_management_provider.dart
@@ -1,40 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_ui_class/models/card_data_model.dart';
+import 'package:flutter_ui_class/models/task_model.dart';
+import 'package:flutter_ui_class/repository/task_repository.dart';
 
-class TaskManagementProvider with ChangeNotifier{
+class TaskManagementProvider with ChangeNotifier {
+  final TaskRepository _repo = TaskRepository();
 
+  Stream<List<TaskModel>> get tasksStream => _repo.getTasks();
 
-  List<CardDataModel> tasks = [
-    CardDataModel(title: "Task 1", subtitle: "This is the first task"),
-    CardDataModel(title: "Task 2", subtitle: "This is the second task", icon: Icons.abc_rounded),
-    CardDataModel(title: "Task 3", subtitle: "This is the third task", icon: Icons.account_balance),
-    CardDataModel(title: "Task 4", subtitle: "This is the fourth task", icon: Icons.add),
-    CardDataModel(title: "Task 5", subtitle: "This is the fifth task", icon: Icons.delete),
-    CardDataModel(title: "Task 6", subtitle: "Custom TASK", icon: Icons.edit),
-  ];
-
-  void addTaskExternal(CardDataModel task){
-    tasks.add(task);
-    notifyListeners();  
-  }
-
-
-  void printTaskCount(){
-    print("Total tasks: ${tasks.length}");
-  }
-
-
-  void addTaskAuto(){
-    tasks.add(
-      CardDataModel(
-        title: "Task ${tasks.length + 1}",
-        subtitle: "This is task ${tasks.length + 1}",
-        icon: Icons.auto_fix_normal
-      )
+  Future<void> addTask(String title, String description) async {
+    final task = TaskModel(
+      id: '',
+      title: title,
+      description: description,
+      createdAt: DateTime.now(),
     );
 
-    print("Added Task ${tasks.length}");
-    print(tasks.last);
-    notifyListeners();
+    await _repo.addTask(task);
+  }
+
+  Future<void> deleteTask(String id) async {
+    await _repo.deleteTask(id);
   }
 }
